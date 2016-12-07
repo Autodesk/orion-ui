44b26b3bddf45df9124806d08fe4bbfd584ac5fe
Sean Durham <sean@sdurham.net>
2016-12-07 11:40:29 -0800

    Base styles on eslint-config-aribnb

--- packages/eslint-config-orion/index.js
+++ packages/eslint-config-orion/index.js
@@ -1,36 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "installedESLint": true,
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "extends": "eslint-config-airbnb",
 }

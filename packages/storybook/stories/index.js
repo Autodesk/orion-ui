b81a6ef122982972ebd0fd6ede3bafaeab1891b7
Sean Durham <sean@sdurham.net>
2016-12-08 12:53:36 -0800

    Add knobs support to storybook #11 (#18)
    Closes #11

--- packages/storybook/stories/index.js
+++ packages/storybook/stories/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
+import { withKnobs, text, select } from '@kadira/storybook-addon-knobs';
+
+const colorOptions = {
+  '#000000': 'Black',
+  '#ffffff': 'White',
+};
 
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .add('with text', () => (
-    <button onClick={action('clicked')}>Hello Button</button>
-  ))
-  .add('with some emoji', () => (
-    <button onClick={action('clicked')}>😀 😎 👍 💯</button>
-  ));
+    <button
+      style={{
+        appearance: 'none',
+        border: '1px solid #999',
+        color: select('Color', colorOptions, '#000000'),
+        backgroundColor: select('Background Color', colorOptions, '#ffffff'),
+      }} onClick={action('clicked')}
+    >{text('Text', 'Hello, button!')}</button>
+));

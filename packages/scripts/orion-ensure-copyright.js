e737960f95f9db8443dec94f8da3bed07e57f086
Sean Durham <sean@sdurham.net>
2016-12-16 14:53:22 -0800

    Ignore files in vendor dirs

--- packages/scripts/orion-ensure-copyright.js
+++ packages/scripts/orion-ensure-copyright.js
@@ -54,7 +54,7 @@ function hasCopyrightNotice(file) {
 }
 
 function ensureCopyright(dir) {
-  const files = glob.sync(path.join(dir, '**/*.js'), { ignore: ['**/node_modules/**'] });
+  const files = glob.sync(path.join(dir, '**/*.js'), { ignore: ['**/node_modules/**', '**/vendor/**'] });
   const result = files.reduce((memo, file) => {
     if (hasCopyrightNotice(file)) {
       memo.pass.push(file);

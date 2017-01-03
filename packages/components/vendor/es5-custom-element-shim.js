3b5eab6050b4dfd6c21c03dd999efe8533d8aea4
Cameron Westland <camwest@gmail.com>
2017-01-03 12:52:15 -0800

    Convert es5-custom-element-shim to be compatible with uglify
    Makes it possible to run `npm run build` inside packages/react-playground

--- packages/components/vendor/es5-custom-element-shim.js
+++ packages/components/vendor/es5-custom-element-shim.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
 * @license
 * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
@@ -57,87 +59,7 @@
 *  Compiling valid class-based custom elements to ES5 will satisfy these
 *  requirements with the latest version of popular transpilers.
 */
-(() => {
-  'use strict';
 
-  if (!window.customElements) {
-    console.warn('Custom elements are not available');
-    return;
-  }
+var code = "(() => {\n  'use strict';\n\n  if (!window.customElements) {\n    console.warn('Custom elements are not available');\n    return;\n  }\n\n  const NativeHTMLElement = window.HTMLElement;\n  const nativeDefine = window.customElements.define;\n  const nativeGet = window.customElements.get;\n  /**\n  * Map of user-provided constructors to tag names.\n  *\n  * @type {Map<Function, string>}\n  */\n  const tagnameByConstructor = new Map();\n  /**\n  * Map of tag names to user-provided constructors.\n  *\n  * @type {Map<string, Function>}\n  */\n  const constructorByTagname = new Map();\n  /**\n  * Whether the constructors are being called by a browser process, ie parsing\n  * or createElement.\n  */\n  let browserConstruction = false;\n  /**\n  * Whether the constructors are being called by a user-space process, ie\n  * calling an element constructor.\n  */\n  let userConstruction = false;\n  window.HTMLElement = function() {\n   if (!browserConstruction) {\n     const tagname = tagnameByConstructor.get(this.constructor);\n     const fakeClass = nativeGet.call(window.customElements, tagname);\n     // Make sure that the fake constructor doesn't call back to this constructor\n     userConstruction = true;\n     const instance = new (fakeClass)();\n     return instance;\n   }\n   // Else do nothing. This will be reached by ES5-style classes doing\n   // HTMLElement.call() during initialization\n   browserConstruction = false;\n  };\n  // By setting the patched HTMLElement's prototype property to the native\n  // HTMLElement's prototype we make sure that:\n  //     document.createElement('a') instanceof HTMLElement\n  // works because instanceof uses HTMLElement.prototype, which is on the\n  // ptototype chain of built-in elements.\n  window.HTMLElement.prototype = NativeHTMLElement.prototype;\n  window.customElements.define = (tagname, elementClass) => {\n   const elementProto = elementClass.prototype;\n   const StandInElement = class extends NativeHTMLElement {\n     constructor() {\n       // Call the native HTMLElement constructor, this gives us the\n       // under-construction instance as 'this':\n       super();\n       // The prototype will be wrong up because the browser used our fake\n       // class, so fix it:\n       Object.setPrototypeOf(this, elementProto);\n       if (!userConstruction) {\n         // Make sure that user-defined constructor bottom's out to a do-nothing\n         // HTMLElement() call\n         browserConstruction = true;\n         // Call the user-defined constructor on our instance:\n         elementClass.call(this);\n       }\n       userConstruction = false;\n     }\n   };\n   const standInProto = StandInElement.prototype;\n   StandInElement.observedAttributes = elementClass.observedAttributes;\n   standInProto.connectedCallback = elementProto.connectedCallback;\n   standInProto.disconnectedCallback = elementProto.disconnectedCallback;\n   standInProto.attributeChangedCallback = elementProto.attributeChangedCallback;\n   standInProto.adoptedCallback = elementProto.adoptedCallback;\n   tagnameByConstructor.set(elementClass, tagname);\n   constructorByTagname.set(tagname, elementClass);\n   nativeDefine.call(window.customElements, tagname, StandInElement);\n  };\n  window.customElements.get = (tagname) => constructorByTagname.get(tagname);\n})();";
 
-  const NativeHTMLElement = window.HTMLElement;
-  const nativeDefine = window.customElements.define;
-  const nativeGet = window.customElements.get;
-  /**
-  * Map of user-provided constructors to tag names.
-  *
-  * @type {Map<Function, string>}
-  */
-  const tagnameByConstructor = new Map();
-  /**
-  * Map of tag names to user-provided constructors.
-  *
-  * @type {Map<string, Function>}
-  */
-  const constructorByTagname = new Map();
-  /**
-  * Whether the constructors are being called by a browser process, ie parsing
-  * or createElement.
-  */
-  let browserConstruction = false;
-  /**
-  * Whether the constructors are being called by a user-space process, ie
-  * calling an element constructor.
-  */
-  let userConstruction = false;
-  window.HTMLElement = function() {
-   if (!browserConstruction) {
-     const tagname = tagnameByConstructor.get(this.constructor);
-     const fakeClass = nativeGet.call(window.customElements, tagname);
-     // Make sure that the fake constructor doesn't call back to this constructor
-     userConstruction = true;
-     const instance = new (fakeClass)();
-     return instance;
-   }
-   // Else do nothing. This will be reached by ES5-style classes doing
-   // HTMLElement.call() during initialization
-   browserConstruction = false;
-  };
-  // By setting the patched HTMLElement's prototype property to the native
-  // HTMLElement's prototype we make sure that:
-  //     document.createElement('a') instanceof HTMLElement
-  // works because instanceof uses HTMLElement.prototype, which is on the
-  // ptototype chain of built-in elements.
-  window.HTMLElement.prototype = NativeHTMLElement.prototype;
-  window.customElements.define = (tagname, elementClass) => {
-   const elementProto = elementClass.prototype;
-   const StandInElement = class extends NativeHTMLElement {
-     constructor() {
-       // Call the native HTMLElement constructor, this gives us the
-       // under-construction instance as `this`:
-       super();
-       // The prototype will be wrong up because the browser used our fake
-       // class, so fix it:
-       Object.setPrototypeOf(this, elementProto);
-       if (!userConstruction) {
-         // Make sure that user-defined constructor bottom's out to a do-nothing
-         // HTMLElement() call
-         browserConstruction = true;
-         // Call the user-defined constructor on our instance:
-         elementClass.call(this);
-       }
-       userConstruction = false;
-     }
-   };
-   const standInProto = StandInElement.prototype;
-   StandInElement.observedAttributes = elementClass.observedAttributes;
-   standInProto.connectedCallback = elementProto.connectedCallback;
-   standInProto.disconnectedCallback = elementProto.disconnectedCallback;
-   standInProto.attributeChangedCallback = elementProto.attributeChangedCallback;
-   standInProto.adoptedCallback = elementProto.adoptedCallback;
-   tagnameByConstructor.set(elementClass, tagname);
-   constructorByTagname.set(tagname, elementClass);
-   nativeDefine.call(window.customElements, tagname, StandInElement);
-  };
-  window.customElements.get = (tagname) => constructorByTagname.get(tagname);
-})();
+eval(code);

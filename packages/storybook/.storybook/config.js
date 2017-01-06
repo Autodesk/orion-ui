fe3c70f009bccab405f0ee3ea59e0d350dd11b40
Cameron Westland <camwest@gmail.com>
2017-01-06 11:11:05 -0800

    Select storybook specs (#43)

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@kadira/storybook';
 
 function loadStories() {
   require('../stories/index.js');
+  require('../stories/select');
   // You can require as many stories as you need.
 }
 

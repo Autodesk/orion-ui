451bfbeba6b9d0a05e00be9afdeb764920f7652b
Anna Neyzberg <anna@carbonfive.com>
2017-01-31 14:24:22 -0800

    Add quick-start instructions

--- packages/landing-page/src/App.js
+++ packages/landing-page/src/App.js
@@ -1,5 +1,7 @@
 import 'tachyons';
 import React, { Component } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/styles';
 import '../vendor/custom-elements-polyfill';
 import { Button, Select } from '@orion-ui/react/lib/2016-12-01';
 import './App.css';
@@ -8,13 +10,94 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = { framework: 'react' };
+    this.state = { selectedIndex: 0 };
 
     this.setFramework = this.setFramework.bind(this);
   }
 
-  setFramework() {
+  setFramework(event) {
+    this.setState({ selectedIndex: event.detail.state.selectedIndex });
+  }
 
+  renderInstallInstructions(framework) {
+    if (framework === 'react') {
+      const installExample = `npm install @orion-ui/react --save-dev`;
+      const usageExample = `
+import Button from '@orion-ui/react/button';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Button>Hello, world!</Button>, document.body);
+      `;
+
+      return (
+        <div>
+          <SyntaxHighlighter
+            language="bash"
+            style={docco}
+          >{installExample}
+          </SyntaxHighlighter>
+          <SyntaxHighlighter
+            language="javascript"
+            style={docco}
+          >{usageExample}
+          </SyntaxHighlighter>
+        </div>
+      );
+    } else {
+      const installExample = `npm install @orion-ui/angular --save-dev`;
+      const usageHTML = `
+<!doctype html>
+<html>
+<body ng-app="app">
+  <div ng-controller="DemoController as ctrl">
+    <orion-button background="ctrl.button.background" color="ctrl.button.color" ng-click="ctrl.action('clicked')">
+      {{ctrl.button.label}}
+    </orion-button>
+  </div>
+</body>
+</html>
+      `;
+      const usageJS = `
+import 'angular';
+import '@orion-ui/angular/lib/2016-12-01';
+
+angular
+  .module('app', ['orion']) // include orion module
+  .controller('DemoController', function() {
+    var ctrl = this;
+
+    ctrl.button = {
+      label: 'Hello, world!',
+      color: 'white',
+      background: 'black'
+    };
+
+    ctrl.action = function(arg) {
+      alert(arg);
+    }
+  });
+      `;
+
+      return (
+        <div>
+          <SyntaxHighlighter
+            language="bash"
+            style={docco}
+          >{installExample}
+          </SyntaxHighlighter>
+          <SyntaxHighlighter
+            language="html"
+            style={docco}
+          >{usageHTML}
+          </SyntaxHighlighter>
+          <SyntaxHighlighter
+            language="javascript"
+            style={docco}
+          >{usageJS}
+          </SyntaxHighlighter>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -22,6 +105,7 @@ class App extends Component {
       { label: 'React', value: 'react', key: 1 },
       { label: 'Angular', value: 'angular', key: 2 },
     ];
+    const selectedFramework = frameworks[this.state.selectedIndex].value;
 
     return (
       <div>
@@ -34,14 +118,17 @@ class App extends Component {
         </div>
         <div>
           <h2 className="f2">Get Started</h2>
-          <ol>
-            <li>
-              Install the library for
-              <Select options={frameworks} onChange={this.setFramework} />
-            </li>
-            <li>Import a component</li>
-            <li>Ready for production!</li>
-          </ol>
+          <div>
+            <ol>
+              <li>
+                Install the library for
+                <Select options={frameworks} selectedIndex={this.state.selectedIndex} onChange={this.setFramework} />
+              </li>
+              <li>Import a component</li>
+              <li>Ready for production!</li>
+            </ol>
+            {this.renderInstallInstructions(selectedFramework)}
+          </div>
         </div>
       </div>
     );

3bbe9a530405dd1ac53f229c8c7166d9031f8e4e
Sean Durham <sean@sdurham.net>
2017-01-31 16:39:00 -0800

    Style landing page

--- packages/landing-page/src/App.js
+++ packages/landing-page/src/App.js
@@ -1,9 +1,10 @@
-import 'tachyons';
-import React, { Component } from 'react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/styles';
 import '../vendor/custom-elements-polyfill';
+import React, { Component } from 'react';
+import 'tachyons';
 import { Button, Select } from '@orion-ui/react/lib/2016-12-01';
+import Nav from './components/nav';
+import GetStarted from './components/get-started';
+import FeaturesBenefits from './components/features-benefits';
 import './App.css';
 
 class App extends Component {
@@ -19,87 +20,6 @@ class App extends Component {
     this.setState({ selectedIndex: event.detail.state.selectedIndex });
   }
 
-  renderInstallInstructions(framework) {
-    if (framework === 'react') {
-      const installExample = `npm install @orion-ui/react --save-dev`;
-      const usageExample = `
-import Button from '@orion-ui/react/button';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<Button>Hello, world!</Button>, document.body);
-      `;
-
-      return (
-        <div>
-          <SyntaxHighlighter
-            language="bash"
-            style={docco}
-          >{installExample}
-          </SyntaxHighlighter>
-          <SyntaxHighlighter
-            language="javascript"
-            style={docco}
-          >{usageExample}
-          </SyntaxHighlighter>
-        </div>
-      );
-    } else {
-      const installExample = `npm install @orion-ui/angular --save-dev`;
-      const usageHTML = `
-<!doctype html>
-<html>
-<body ng-app="app">
-  <div ng-controller="DemoController as ctrl">
-    <orion-button background="ctrl.button.background" color="ctrl.button.color" ng-click="ctrl.action('clicked')">
-      {{ctrl.button.label}}
-    </orion-button>
-  </div>
-</body>
-</html>
-      `;
-      const usageJS = `
-import 'angular';
-import '@orion-ui/angular/lib/2016-12-01';
-
-angular
-  .module('app', ['orion']) // include orion module
-  .controller('DemoController', function() {
-    var ctrl = this;
-
-    ctrl.button = {
-      label: 'Hello, world!',
-      color: 'white',
-      background: 'black'
-    };
-
-    ctrl.action = function(arg) {
-      alert(arg);
-    }
-  });
-      `;
-
-      return (
-        <div>
-          <SyntaxHighlighter
-            language="bash"
-            style={docco}
-          >{installExample}
-          </SyntaxHighlighter>
-          <SyntaxHighlighter
-            language="html"
-            style={docco}
-          >{usageHTML}
-          </SyntaxHighlighter>
-          <SyntaxHighlighter
-            language="javascript"
-            style={docco}
-          >{usageJS}
-          </SyntaxHighlighter>
-        </div>
-      );
-    }
-  }
-
   render() {
     const frameworks = [
       { label: 'React', value: 'react', key: 1 },
@@ -108,27 +28,40 @@ angular
     const selectedFramework = frameworks[this.state.selectedIndex].value;
 
     return (
-      <div>
-        <div className="sp5">
+      <div className="mw9 center ph4 pb6">
+        <Nav />
+        <div className="pv5">
           <h1 className="f1">Orion</h1>
           <p>A resusable, HIG compliant, UI component library for Autodesk applications based on web standards and open source best pracices.</p>
         </div>
-        <div className="sp5">
-          <Button>See All Components</Button>
+        <div className="pv3 center w-20">
+          <Button size="large">See All Components</Button>
         </div>
-        <div>
+        <div className="pv3">
           <h2 className="f2">Get Started</h2>
-          <div>
-            <ol>
-              <li>
+          <div className="fx-row-l">
+            <ol className="fx-g1">
+              <li className="f4 pv3 fx-row fai-c">
+                <span className="w-10 f1">1</span>
                 Install the library for
+                <span className="mr1"></span>
                 <Select options={frameworks} selectedIndex={this.state.selectedIndex} onChange={this.setFramework} />
               </li>
-              <li>Import a component</li>
-              <li>Ready for production!</li>
+              <li className="f4 pv3 fx-row fai-c">
+                <span className="w-10 f1">2</span>
+                Import a component
+              </li>
+              <li className="f4 pv3 fx-row fai-c">
+                <span className="w-10 f1">3</span>
+                Ready for production!
+              </li>
             </ol>
-            {this.renderInstallInstructions(selectedFramework)}
+            <GetStarted framework={selectedFramework} className="fx-g2"/>
           </div>
+        </div>
+        <div>
+          <h2 className="f2">Features & Benefits</h2>
+          <FeaturesBenefits />
         </div>
       </div>
     );

3274ac95b92f9c82534607c31e50d174ad60137f
Cameron Westland <camwest@gmail.com>
2017-02-07 08:18:17 -0800

    Copyright ignores examples directory and .d.ts files

--- packages/scripts/orion-ensure-copyright.js
+++ packages/scripts/orion-ensure-copyright.js
@@ -54,7 +54,7 @@ function hasCopyrightNotice(file) {
 }
 
 function ensureCopyright(dir) {
-  const files = glob.sync(path.join(dir, '**/*.{js,ts}'), { ignore: ['**/node_modules/**', '**/bower_components/**', '**/coverage/**', '**/vendor/**', 'gemini-report/**'] });
+  const files = glob.sync(path.join(dir, '**/*.{js,ts}'), { ignore: ['**/*.d.ts', '**/node_modules/**', '**/bower_components/**', '**/coverage/**', '**/examples/**', '**/vendor/**', 'gemini-report/**'] });
   const result = files.reduce((memo, file) => {
     if (hasCopyrightNotice(file)) {
       memo.pass.push(file);

cfb6720d2d77806c12c50bb5d076fec8341dac9a
Cameron Westland <camwest@gmail.com>
2017-04-14 16:57:50 -0700

    remove un-used var

--- packages/hig-custom-fiber-renderer/src/index.js
+++ packages/hig-custom-fiber-renderer/src/index.js
@@ -16,7 +16,7 @@ limitations under the License.
 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HIG, { Button, Menu, Slot } from './hig-react';
+import HIG, { Menu, Slot } from './hig-react';
 
 class App extends React.Component {
   constructor() {

315bb4cd1c6fece113c8836e190978b564216309
Cameron Westland <camwest@gmail.com>
2017-04-18 10:50:44 -0700

    add react-hig which works without fiber

--- packages/hig-custom-fiber-renderer/src/react-hig-fiber.js
+++ packages/hig-custom-fiber-renderer/src/react-hig-fiber.js
@@ -28,7 +28,8 @@ import {
   unmountComponentAtNode
 } from 'react-dom';
 
-import { createElement } from './hig-react-elements';
+import { createElement } from './react-hig-elements';
+import prepareUpdate from './prepareUpdate';
 
 /**
  * HIG Fiber Renderer
@@ -132,51 +133,7 @@ const HIGRenderer = ReactFiberReconciler({
     rootContainerInstance,
     hostContext
   ) {
-    // example
-    // const oldProps = { label: 'foo' }
-    // const newProps = { label: 'bar', onClick: 'foo' };
-
-    const updatePayload = [];
-    const mergedProps = {};
-
-    // Fill out the initial props
-    for (const propKey in oldProps) {
-      mergedProps[propKey] = [oldProps[propKey], null];
-    }
-
-    // example
-    // mergedProps =  { label: ['foo', null] }
-
-    // Now overwrite the changes
-    for (const propKey in newProps) {
-      // When prop exists overwrite the value
-      if (mergedProps[propKey] !== undefined) {
-        mergedProps[propKey][1] = newProps[propKey];
-      } else {
-        mergedProps[propKey] = [null, newProps[propKey]];
-      }
-    }
-
-    // example
-    // mergedProps = { label: ['foo', 'bar'], onClick: [null, 'foo']}
-
-    // Compare differences
-    for (const propKey in mergedProps) {
-      const [oldVal, newVal] = mergedProps[propKey];
-
-      if (oldVal !== newVal) {
-        updatePayload.push(propKey, newVal);
-      }
-    }
-
-    // example
-    // updatePayload = ['label', 'bar', 'onClick', 'foo']
-
-    if (updatePayload.length === 0) {
-      return null;
-    } else {
-      return updatePayload;
-    }
+    return prepareUpdate(oldProps, newProps);
   },
 
   commitUpdate(

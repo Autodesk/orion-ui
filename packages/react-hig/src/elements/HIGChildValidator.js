70938041e2aa7c12dcc3a69759bb9fcbe84bf17a
Cameron Westland <camwest@gmail.com>
2017-05-18 16:18:23 -0700

    add some docs

--- packages/react-hig/src/elements/HIGChildValidator.js
+++ packages/react-hig/src/elements/HIGChildValidator.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
+/**
+ * Returns the displayName of a React Component
+ *
+ * @param {React.Component} Component
+ */
+const getDisplayName = Component =>
+  Component.displayName ||
+  Component.name ||
+  (typeof Component === 'string' ? Component : 'Component');
+
+/**
+ * Create a PropType function which checks against a known set of valid child nodes
+ * Useful for validating the child node types of a HIG Component.
+ *
+ * @param {React.Component[]} validChildren an array of React.Component constructors
+ *
+ * @example
+ *
+ * GlobalNav.propTypes = {
+ *  children: HIGChildValidator([SideNav, Container])
+ * }
+ *
+ */
 const HIGChildValidator = validChildren =>
   (props, propName, componentName) => {
     const prop = props[propName];
 
     let error = null;
     React.Children.forEach(prop, function(child) {
-      const getDisplayName = Component =>
-        Component.displayName ||
-        Component.name ||
-        (typeof Component === 'string' ? Component : 'Component');
-
       const childDisplayName = getDisplayName(child.type || child);
 
       if (validChildren.indexOf(child.type) === -1) {
